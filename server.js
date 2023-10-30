8d51ca719b832d36d3a7ee29fd5eee4b82c7cbd4
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-10-30 00:43:51 -0600

    added: using mongodb added images to my db using the image

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from "express";
 import { v4 } from 'uuid'
+import mongoose from 'mongoose'
 
 const app = express();
 
-app.get("/", (req, res) => {
+const db = await mongoose.connect('mongodb://mymongo/testdb')
+console.log(db.connection.db.databaseName)
+
+const productSchema = mongoose.Schema({
+  name: String,
+})
+
+const ProductModel = mongoose.model('Product', productSchema)
+
+app.get("/", async (req, res) => {
+
+  const newProduct = await ProductModel.create({
+    name: 'Product 1'
+  })
+
   res.json({
-    id: v4()
+    id: v4(),
+    newProduct
   })
 });
 
